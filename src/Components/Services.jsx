99d50854a1e9151c47e3d821dd7fdc296da190f7
Fixigo-HS <shaikyams906@gmail.com>
2025-02-27 14:37:17 +0530

    Some small changes

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -22,7 +22,7 @@ const Services = () => {
           <motion.div
             key={index}
             initial={{ opacity: 0, scale: 0.8 }}
-            whileInView={{ opacity: 1, scale: 1 }}
+            animate={{ opacity: 1, scale: 1 }}
             whileHover={{ scale: 1.05 }}
             transition={{ duration: 0.5, delay: index * 0.2 }}
             className="service-card"
