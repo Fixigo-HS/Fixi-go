185200f61cd7703be15ae25d4a8e113d8e274cb7
patan1213 <patanasif616@gmail.com>
2025-03-05 15:58:00 +0530

    footer some additional Adjustments

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
-import { FaInstagram, FaFacebook, FaTwitter } from 'react-icons/fa'; // Import icons
+import { FaInstagram, FaFacebook } from 'react-icons/fa'; // Import Instagram and Facebook icons
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="social-icons">
         {/* Instagram Icon */}
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-          <FaInstagram className="icon" />
+        <a href="https://www.instagram.com/fixi_go/?next=%2F" target="_blank" rel="noopener noreferrer">
+          <FaInstagram className="icon instagram" />
         </a>
         {/* Facebook Icon */}
-        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-          <FaFacebook className="icon" />
+        <a href="https://www.facebook.com/profile.php?id=61573795413881" target="_blank" rel="noopener noreferrer">
+          <FaFacebook className="icon facebook" />
         </a>
-        {/* Twitter Icon */}
-        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-          <FaTwitter className="icon" />
+        {/* X (Twitter) Icon */}
+        <a href="https://x.com/Fixi_go" target="_blank" rel="noopener noreferrer">
+          <svg
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            fill="currentColor"
+            className="icon x-logo"
+          >
+            <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+          </svg>
         </a>
         <p>&copy; {new Date().getFullYear()} Fixigo. All rights reserved.</p>
       </div>
